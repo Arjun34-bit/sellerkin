9e39ee028ad50c0d0133f6bd67ac94d76b460f02
Arjun34-bit <arjundevendra34@gmail.com>
2023-12-26 00:06:25 +0530

    root

--- server/index.js
+++ server/index.js
@@ -33,3 +33,4 @@ if (process.env.NODE_ENV === "production") {
 app.listen("5000", console.log(`Server Started at Port : ${PORT}`.yellow.bold));
 
 // Product.insertMany(listings);
+export default app;
